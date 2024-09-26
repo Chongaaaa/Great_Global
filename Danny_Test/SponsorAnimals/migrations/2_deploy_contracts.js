20f6dcc7c3a4d24995b79ff0c15fe7b8f63df55b
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2024-09-26 08:13:09 +0800

    eheheh

--- Danny_Test/SponsorAnimals/migrations/2_deploy_contracts.js
+++ Danny_Test/SponsorAnimals/migrations/2_deploy_contracts.js
@@ -2,16 +2,22 @@ var Claims = artifacts.require("./ClaimProcessing.sol");
 var Users = artifacts.require("./UserAuth.sol");
 var PaymentModule = artifacts.require("./PaymentModule.sol");
 var AdminInsurancePolicy = artifacts.require("./AdminInsurancePolicy.sol");
+var PurchasePackage = artifacts.require("./PurchasePackage.sol");
 
 module.exports = async function (deployer) {
   // Deploy UserAuth first
   await deployer.deploy(Users);
   const userAuthInstance = await Users.deployed();
 
+  // Now deploy AdminInsurancePolicy, passing the address of UserAuth
+  await deployer.deploy(AdminInsurancePolicy, userAuthInstance.address);
+  const adminInsuraceInstance = await AdminInsurancePolicy.deployed();
+
+  await deployer.deploy(PurchasePackage, userAuthInstance.address, adminInsuraceInstance.address);
+
   // Deploy other contracts
   await deployer.deploy(Claims);
   await deployer.deploy(PaymentModule);
-  
-  // Now deploy AdminInsurancePolicy, passing the address of UserAuth
-  await deployer.deploy(AdminInsurancePolicy, userAuthInstance.address);
+
+
 };
